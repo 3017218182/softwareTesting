f21e6a711f537a84a548b9e5d0db6595d3ce6019
3017218182 <3017218182@tju.edu.cn>
2020-03-08 15:14:15 +0800

    change MoneyTesting.java

--- lab1/test/classes/tju/stu/lab1/MoneyTesting.java
+++ lab1/test/classes/tju/stu/lab1/MoneyTesting.java
@@ -2,12 +2,35 @@ package classes.tju.stu.lab1;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class MoneyTesting {
-
-	Money mon = null;
+	@Parameters
+	public static Collection<Object[]> getData(){
+		return Arrays.asList(new Object[][]{
+			{93,true},
+			{94,false},
+			{0,true},
+			{-1,false}
+		});
+	}
+	
+	private int input;
+	private boolean expected;
+	private Money mon = null;
+	
+	public MoneyTesting(int input,boolean expected) {
+		this.input=input;
+		this.expected=expected;
+	}
 	
 	@Before
 	public void setup() {
@@ -16,7 +39,8 @@ public class MoneyTesting {
 	
 	@Test
 	public void testMoney() {
-		assertEquals(false,mon.calculate(-1));
+		assertEquals(this.expected,mon.calculate(this.input));
 	}
-	
+
 }
+
